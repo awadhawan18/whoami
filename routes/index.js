b675d9f2becb25c9415b8b54cd43c42bb655fee8
Abhishek Wadhawan <awadhawan18@gmail.com>
2017-06-09 02:27:28 +0530

    ip and express-useragent

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 var express = require('express');
-var useragent = require('os');
+var useragent = require('express-useragent');
 var router = express.Router();
-var os = useragent.type() + " " + useragent.release();
+var os;
+var add = require('ip');
 var ip;
 var language;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    ip = req.ip;
+    ip = add.address();
+    os = useragent.parse(req.headers['user-agent']);
     var arr = req.headers['accept-language'].split(",");
     language = req.acceptsLanguages(arr[0] );
-    res.render('index', { IP: ip, OS: os, LAN:language});
+    res.render('index', { IP: ip, OS: os.os, LAN:language});
 });
 
 module.exports = router;
