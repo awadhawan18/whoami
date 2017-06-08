867f1a1ccec436ff903e2a751d98ae44311b6abc
Abhishek Wadhawan <awadhawan18@gmail.com>
2017-06-09 01:12:40 +0530

    request

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var useragent = require('os');
 var router = express.Router();
+var os = useragent.type() + " " + useragent.release();
+var ip;
+var language;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    ip = req.ip;
+    var arr = req.headers['accept-language'].split(",");
+    language = req.acceptsLanguages(arr[0] );
+    res.render('index', { IP: ip, OS: os, LAN:language});
 });
 
 module.exports = router;
