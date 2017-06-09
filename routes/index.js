ad82f0efd60892df4247523b537a00d658d5d701
Abhishek Wadhawan <awadhawan18@gmail.com>
2017-06-09 12:12:11 +0530

    uses request-ip package now

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,17 @@ var express = require('express');
 var useragent = require('express-useragent');
 var router = express.Router();
 var os;
-var add = require('ip');
-var ip;
+var ip =  require('request-ip');;
 var language;
+var clientIP;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    ip = add.address();
+    clientIP = ip.getClientIp(req);
     os = useragent.parse(req.headers['user-agent']);
     var arr = req.headers['accept-language'].split(",");
     language = req.acceptsLanguages(arr[0] );
-    res.render('index', { IP: ip, OS: os.os, LAN:language});
+    res.render('index', { IP: clientIP, OS: os.os, LAN:language});
 });
 
 module.exports = router;
